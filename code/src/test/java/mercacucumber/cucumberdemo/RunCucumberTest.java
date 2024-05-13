e509d3b008592f91cdcc5b41ab8c5f9d01bce950
Manuel Caballero Rabago <mcabalra@mercadona.es>
2024-05-13 11:37:33 +0200

    fix: Add camelCase configuration and filter by tag

--- code/src/test/java/mercacucumber/cucumberdemo/RunCucumberTest.java
+++ code/src/test/java/mercacucumber/cucumberdemo/RunCucumberTest.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"pretty"}, snippets = CucumberOptions.SnippetType.CAMELCASE)
 public class RunCucumberTest {
 }
